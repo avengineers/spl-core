090857a75bb59303258cd0d69bcd981df67b26d3
Eggert, Matthias (SD-RM) <Matthias.Eggert@marquardt.com>
2022-08-24 13:04:45 +0200

    update spl if already installed, do not fail

--- install.ps1
+++ install.ps1
@@ -6,11 +6,21 @@ param(
     [string]$version = "develop" ## use latest if no verison was given
 )
 
+echo "Clone/Update SPL version: $version"
+
 if (-Not (Get-Command git -ErrorAction SilentlyContinue)) {
     throw "'git' executable not found, please install it."
 }
 
-git clone https://github.com/avengineers/SPL.git --branch $version --depth 1 ./build/spl
+if (Test-Path build/spl/.git) {
+    Push-Location build/spl
+    git fetch --all --tags --prune
+    git checkout $version
+    Pop-Location
+} else {
+    git clone https://github.com/avengineers/SPL.git --branch $version --depth 1 ./build/spl
+}
+
 Push-Location build/spl
 
 . .\powershell\spl-functions.ps1
