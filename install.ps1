8c1f99d2640786fc6a93925fe096ab80205bf59c
Eggert, Matthias (SD-RM) <Matthias.Eggert@marquardt.com>
2022-08-26 15:40:21 +0200

    add possibility to skip dependency installation

--- install.ps1
+++ install.ps1
@@ -7,6 +7,10 @@ param(
         Mandatory = $false
     )]
     [string]$repo_url = "https://github.com/avengineers/SPL.git"
+    , [Parameter(
+        Mandatory = $false
+    )]
+    [switch]$skipInstall
 )
 
 echo "Clone/Update SPL version: $version from $repo_url"
@@ -21,17 +25,21 @@ if (Test-Path build/spl-core/.git) {
 
 git clone $repo_url --branch $version --depth 1 ./build/spl-core
 
-. .\build\spl-core\powershell\spl-variables.ps1
-. .\build\spl-core\powershell\spl-functions.ps1
+if ($skipInstall) {
+    Write-Host "Skipping installation of dependencies."
+} else {
+    . .\build\spl-core\powershell\spl-variables.ps1
+    . .\build\spl-core\powershell\spl-functions.ps1
 
-Push-Location build/spl-core
-Out-File -FilePath $version
+    Push-Location build/spl-core
+    Out-File -FilePath $version
 
-if ($SPL_PROXY_HOST -and $SPL_PROXY_BYPASS_LIST) {
-    Setup-Proxy -ProxyHost $SPL_PROXY_HOST -NoProxy $SPL_PROXY_BYPASS_LIST
+    if ($SPL_PROXY_HOST -and $SPL_PROXY_BYPASS_LIST) {
+        Setup-Proxy -ProxyHost $SPL_PROXY_HOST -NoProxy $SPL_PROXY_BYPASS_LIST
+    }
+
+    Install-Basic-Tools
+    $SPL_INSTALL_DEPENDENCY_JSON_FILE_CONTENT = Get-Content -Raw -Path "dependencies.json" | ConvertFrom-Json
+    Install-Mandatory-Tools -JsonDependencies $SPL_INSTALL_DEPENDENCY_JSON_FILE_CONTENT
+    Pop-Location #build/spl-core
 }
-
-Install-Basic-Tools
-$SPL_INSTALL_DEPENDENCY_JSON_FILE_CONTENT = Get-Content -Raw -Path "dependencies.json" | ConvertFrom-Json
-Install-Mandatory-Tools -JsonDependencies $SPL_INSTALL_DEPENDENCY_JSON_FILE_CONTENT
-Pop-Location #build/spl-core
