b7d61e6f03e726ee42d0f6df3d24f3dcc68d2724
Reichwein, Judith (RDD-RM) <Judith.Reichwein@marquardt.com>
2024-04-12 18:26:36 +0200

    chore: add pypeline to spl_core

--- build.ps1
+++ build.ps1
@@ -1,34 +1,6 @@
 param(
     [switch]$clean ## clean build, wipe out all build artifacts
-    , [switch]$install ## install mandatory packages
 )
-
-Function Invoke-CommandLine {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'Usually this statement must be avoided (https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/avoid-using-invoke-expression?view=powershell-7.3), here it is OK as it does not execute unknown code.')]
-    param (
-        [Parameter(Mandatory = $true, Position = 0)]
-        [string]$CommandLine,
-        [Parameter(Mandatory = $false, Position = 1)]
-        [bool]$StopAtError = $true,
-        [Parameter(Mandatory = $false, Position = 2)]
-        [bool]$Silent = $false
-    )
-    if (-Not $Silent) {
-        Write-Output "Executing: $CommandLine"
-    }
-    $global:LASTEXITCODE = 0
-    Invoke-Expression $CommandLine
-    if ($global:LASTEXITCODE -ne 0) {
-        if ($StopAtError) {
-            Write-Error "Command line call `"$CommandLine`" failed with exit code $global:LASTEXITCODE"
-        }
-        else {
-            if (-Not $Silent) {
-                Write-Output "Command line call `"$CommandLine`" failed with exit code $global:LASTEXITCODE, continuing ..."
-            }
-        }
-    }
-}
 
 # Update/Reload current environment variable PATH with settings from registry
 Function Initialize-EnvPath {
@@ -64,19 +36,13 @@ Push-Location $PSScriptRoot
 Write-Output "Running in ${pwd}"
 
 try {
-    if ($install) {
-        Invoke-Bootstrap
-    }
-    else {
-        if (Test-RunningInCIorTestEnvironment -or $Env:USER_PATH_FIRST) {
-            Initialize-EnvPath
-        }
-        # Execute all tests
-        .\.venv\Scripts\poetry run pytest
+    # bootstrap environment
+    Invoke-Bootstrap
 
-        # Build documentation
-        .\.venv\Scripts\poetry run sphinx-build docs out/docs/html
-    }
+    Initialize-EnvPath
+
+    # Run pypeline
+    .\.venv\Scripts\pypeline run
 }
 finally {
     Pop-Location
