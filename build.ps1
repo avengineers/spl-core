2cb82285baed0c19089f6d16b5d391e8a81a47ec
Guenther, Karsten (RDD-RM) <Karsten.Guenther@marquardt.com>
2024-06-21 10:24:19 +0200

    chore: update winlibs package

--- build.ps1
+++ build.ps1
@@ -1,22 +1,54 @@
+<#
+.DESCRIPTION
+    Wrapper for installing dependencies, running and testing the project
+#>
+
 param(
-    [Parameter(Mandatory = $false, HelpMessage = 'Clean build, wipe out all build artifacts. (Switch, default: false)')]
-    [switch]$clean = $false,
-    [Parameter(Mandatory = $false, HelpMessage = 'Install all dependencies required to build. (Switch, default: false)')]
-    [switch]$install = $false
+    [switch]$clean ## clean build, wipe out all build artifacts
+    , [switch]$install ## install mandatory packages
 )
 
-function Test-RunningInCIorTestEnvironment {
-    return [Boolean]($Env:JENKINS_URL -or $Env:PYTEST_CURRENT_TEST -or $Env:GITHUB_ACTIONS)
+function Invoke-CommandLine {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'Usually this statement must be avoided (https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/avoid-using-invoke-expression?view=powershell-7.3), here it is OK as it does not execute unknown code.')]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$CommandLine,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [bool]$StopAtError = $true,
+        [Parameter(Mandatory = $false, Position = 2)]
+        [bool]$PrintCommand = $true,
+        [Parameter(Mandatory = $false, Position = 3)]
+        [bool]$Silent = $false
+    )
+    if ($PrintCommand) {
+        Write-Output "Executing: $CommandLine"
+    }
+    $global:LASTEXITCODE = 0
+    if ($Silent) {
+        # Omit information stream (6) and stdout (1)
+        Invoke-Expression $CommandLine 6>&1 | Out-Null
+    }
+    else {
+        Invoke-Expression $CommandLine
+    }
+    if ($global:LASTEXITCODE -ne 0) {
+        if ($StopAtError) {
+            Write-Error "Command line call `"$CommandLine`" failed with exit code $global:LASTEXITCODE"
+        }
+        else {
+            Write-Output "Command line call `"$CommandLine`" failed with exit code $global:LASTEXITCODE, continuing ..."
+        }
+    }
 }
 
 function Invoke-Bootstrap {
     # Download bootstrap scripts from external repository
-    Invoke-RestMethod https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.6.0/install.ps1 | Invoke-Expression
+    Invoke-RestMethod https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.9.0/install.ps1 | Invoke-Expression
     # Execute bootstrap script
     . .\.bootstrap\bootstrap.ps1
 }
 
-Function Remove-Path {
+function Remove-Path {
     param (
         [Parameter(Mandatory = $true, Position = 0)]
         [string]$path
@@ -32,7 +64,8 @@ Function Remove-Path {
 }
 
 ## start of script
-# Always set the $InformationPreference variable to "Continue" globally, this way it gets printed on execution and continues execution afterwards.
+# Always set the $InformationPreference variable to "Continue" globally,
+# this way it gets printed on execution and continues execution afterwards.
 $InformationPreference = "Continue"
 
 # Stop on first error
@@ -42,9 +75,7 @@ Push-Location $PSScriptRoot
 Write-Output "Running in ${pwd}"
 
 try {
-    # clean build
     if ($clean) {
-        Remove-Path "build"
         Remove-Path ".venv"
     }
 
@@ -52,14 +83,14 @@ try {
     Invoke-Bootstrap
 
     if (-Not $install) {
+        if ($clean) {
+            Remove-Path "build"
+        }
         # Run pypeline
-        .\.venv\Scripts\pypeline run
+        Invoke-CommandLine ".venv\Scripts\pypeline run"
     }
 }
 finally {
     Pop-Location
-    if (-Not (Test-RunningInCIorTestEnvironment)) {
-        Read-Host -Prompt "Press Enter to continue ..."
-    }
 }
 ## end of script
