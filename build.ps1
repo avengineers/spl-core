bb228195040c26d646556aea2ce3c01d1c681b8b
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2024-09-18 23:24:41 +0200

    chore: update to bootstrap 1.13.0

--- build.ps1
+++ build.ps1
@@ -43,7 +43,7 @@ function Invoke-CommandLine {
 
 function Invoke-Bootstrap {
     # Download bootstrap scripts from external repository
-    Invoke-RestMethod https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.12.0/install.ps1 | Invoke-Expression
+    Invoke-RestMethod https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.13.0/install.ps1 | Invoke-Expression
     # Execute bootstrap script
     . .\.bootstrap\bootstrap.ps1
 }
