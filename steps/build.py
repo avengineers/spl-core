2ac1144db52edfe0e2dad9ec74c4d540e029c72a
Guenther, Karsten (RDDC-RM) <Karsten.Guenther@marquardt.com>
2025-01-14 16:45:12 +0100

    chore: markers for integration tests

--- steps/build.py
+++ steps/build.py
@@ -22,10 +22,11 @@ class RunPytest(PipelineStep):
     def update_execution_context(self) -> None:
         pass
 
+
 class GenerateDocs(PipelineStep):
     def run(self) -> None:
         logger.info(f"{self.get_name()}")
-        self.execution_context.create_process_executor(["poetry", "run", "sphinx-build docs out/docs/html"]).execute()
+        self.execution_context.create_process_executor(["poetry", "run", "sphinx-build", "docs", "out/docs/html"]).execute()
 
     def get_inputs(self) -> List[Path]:
         return []
