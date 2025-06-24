296160fe7d1beeb2e29cc6de9e4026cd2dcc804d
cuinixam <22344617+cuinixam@users.noreply.github.com>
2025-06-24 15:12:32 +0200

    fix: bootstrap fails due to pip-system-certs incompatible version

--- src/spl_core/kickstart/templates/project/build.ps1
+++ src/spl_core/kickstart/templates/project/build.ps1
@@ -261,7 +261,7 @@ function Get-User-Menu-Selection {
 
 function Invoke-Bootstrap {
     # Download bootstrap scripts from external repository
-    Invoke-RestMethod -Uri https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.17.0/install.ps1 | Invoke-Expression
+    Invoke-RestMethod -Uri https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.17.1/install.ps1 | Invoke-Expression
     # Execute bootstrap script
     . .\.bootstrap\bootstrap.ps1
 }
