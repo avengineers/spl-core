4b9343df5895c4359c4bc2244f5971f06cdcee6a
Reichwein, Judith (RDDC-RM) <Judith.Reichwein@marquardt.com>
2026-03-16 14:06:36 +0100

    feat: refactor  report generation

---
+++ src/spl_core/report_generation/spl_html_settings.py
@@ -0,0 +1,23 @@
+"""HTML theme configuration for SPL projects."""
+
+# HTML Theme
+html_theme = "sphinx_rtd_theme"
+
+# Show source links
+html_show_sourcelink = True
+
+# Read the Docs Theme Options
+html_theme_options = {
+    "canonical_url": "",
+    "analytics_id": "",
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": True,
+    "logo_only": False,
+    "style_nav_header_background": "white",
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 6,
+    "includehidden": True,
+    "titles_only": False,
+}
